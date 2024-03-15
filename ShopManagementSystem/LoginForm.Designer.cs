a72fa7b6e9b72e95a4b73af5fa2fbb0660088ebb
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2024-03-16 02:26:28 +0600

    set logo,header title,main picture

--- ShopManagementSystem/LoginForm.Designer.cs
+++ ShopManagementSystem/LoginForm.Designer.cs
@@ -34,11 +34,19 @@
             label2 = new Label();
             textBox2 = new TextBox();
             login = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.Red;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(pictureBox2);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
@@ -47,7 +55,7 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(41, 198);
+            textBox1.Location = new Point(46, 245);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(237, 31);
             textBox1.TabIndex = 1;
@@ -55,7 +63,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(41, 159);
+            label1.Location = new Point(46, 206);
             label1.Name = "label1";
             label1.Size = new Size(108, 25);
             label1.TabIndex = 2;
@@ -64,7 +72,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(41, 257);
+            label2.Location = new Point(46, 304);
             label2.Name = "label2";
             label2.Size = new Size(96, 25);
             label2.TabIndex = 4;
@@ -72,7 +80,7 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(41, 296);
+            textBox2.Location = new Point(46, 343);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(237, 31);
             textBox2.TabIndex = 3;
@@ -80,20 +88,50 @@
             // login
             // 
             login.BackColor = Color.Red;
+            login.FlatAppearance.BorderSize = 0;
+            login.FlatStyle = FlatStyle.Flat;
             login.Font = new Font("Segoe UI Black", 9F);
             login.ForeColor = SystemColors.ButtonFace;
-            login.Location = new Point(41, 353);
+            login.Location = new Point(46, 400);
             login.Name = "login";
             login.Size = new Size(237, 34);
             login.TabIndex = 5;
             login.Text = "LOGIN";
             login.UseVisualStyleBackColor = false;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(107, 90);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(93, 75);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(13, 5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(50, 50);
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonFace;
+            label3.Location = new Point(73, 18);
+            label3.Name = "label3";
+            label3.Size = new Size(239, 25);
+            label3.TabIndex = 7;
+            label3.Text = "Shop Management System";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(320, 550);
+            Controls.Add(pictureBox1);
             Controls.Add(login);
             Controls.Add(label2);
             Controls.Add(textBox2);
@@ -104,6 +142,10 @@
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -116,5 +158,8 @@
         private Label label2;
         private TextBox textBox2;
         private Button login;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label3;
     }
 }
